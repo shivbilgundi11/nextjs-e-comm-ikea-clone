df9c1de80f78e12a661c5fa8eaf36fb9ccdb7949
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-07-15 23:19:24 +0530

    ui: add footer component

---
+++ app/_components/footer-links-col.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useState } from 'react';
+import { IoIosArrowUp } from 'react-icons/io';
+
+interface LinkItem {
+  id: string;
+  footerLink: string;
+}
+
+interface LinksList {
+  id: number;
+  section: string;
+  items: LinkItem[];
+}
+
+export default function FooterLinkCol({ linksList }: { linksList: LinksList }) {
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <>
+      <div className='h-full w-full'>
+        <div className='hidden w-full lg:block'>
+          <h5 className='mb-8 text-[15px] font-semibold'>
+            {linksList.section}
+          </h5>
+
+          <ul className='flex h-full flex-col gap-y-4'>
+            {linksList.items.map((link) => {
+              return (
+                <li
+                  key={link.id}
+                  className='cursor-pointer text-sm text-[#484848] hover:underline'
+                >
+                  {link.footerLink}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className='border-y py-10 lg:hidden'>
+          <div
+            className={`group ${showLinks ? 'mb-8' : 'mb-0'} flex h-full cursor-pointer items-center justify-between`}
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <h5 className='text-[15px] font-semibold group-hover:underline'>
+              {linksList.section}
+            </h5>
+
+            <span
+              className={`text-2xl font-bold transition-all duration-300 ease-in-out ${!showLinks ? 'rotate-180' : 'rotate-0'}`}
+            >
+              <IoIosArrowUp />
+            </span>
+          </div>
+          <ul
+            className={`h-full ${showLinks ? 'flex' : 'hidden'} flex-col gap-y-6`}
+          >
+            {linksList.items.map((link) => {
+              return (
+                <li
+                  key={link.id}
+                  className='cursor-pointer text-sm text-[#484848] hover:underline'
+                >
+                  {link.footerLink}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ lib/links.ts
@@ -0,0 +1,58 @@
+// Footer Links
+export const footerLinks = [
+  {
+    id: 1,
+    section: 'IKEA Family',
+    items: [
+      { id: 'fam-4d5e6f', footerLink: 'Log in' },
+      { id: 'fam-7g8h9i', footerLink: 'Join IKEA Family' },
+      { id: 'fam-j0k1l2', footerLink: 'Member offers' },
+      { id: 'fam-3m4n5o', footerLink: 'Workshops & Events' },
+    ],
+  },
+  {
+    id: 2,
+    section: 'Services',
+    items: [
+      { id: 'srv-1a2b3c', footerLink: 'Delivery Service' },
+      { id: 'srv-4d5e6f', footerLink: 'Click & collect' },
+      { id: 'srv-7g8h9i', footerLink: 'Personal shopper' },
+      { id: 'srv-j0k1l2', footerLink: 'Design your room' },
+      { id: 'srv-3m4n5o', footerLink: 'Assembly Service' },
+      { id: 'srv-6p7q8r', footerLink: 'Measuring Service' },
+      { id: 'srv-9s0t1u', footerLink: 'Kitchen Planning' },
+      { id: 'srv-2v3w4x', footerLink: 'Installation Service' },
+      { id: 'srv-5y6z7a', footerLink: 'Track & manage your order' },
+      { id: 'srv-8b9c0d', footerLink: 'Customer Service' },
+      { id: 'srv-1e2f3g', footerLink: 'Recycle Program' },
+    ],
+  },
+  {
+    id: 3,
+    section: 'Help',
+    items: [
+      { id: 'hlp-1a2b3c', footerLink: 'How to shop' },
+      { id: 'hlp-4d5e6f', footerLink: 'Return policy' },
+      { id: 'hlp-7g8h9i', footerLink: 'Prices and price tags' },
+      { id: 'hlp-j0k1l2', footerLink: 'Contact us' },
+      { id: 'hlp-3m4n5o', footerLink: "FAQ's" },
+      { id: 'hlp-6p7q8r', footerLink: 'Planners' },
+      { id: 'hlp-9s0t1u', footerLink: 'Gift Card' },
+      { id: 'hlp-2v3w4x', footerLink: 'Feedback' },
+      { id: 'hlp-5y6z7a', footerLink: 'Terms and conditions' },
+    ],
+  },
+  {
+    id: 4,
+    section: 'About IKEA',
+    items: [
+      { id: 'abt-1a2b3c', footerLink: 'This is IKEA' },
+      { id: 'abt-4d5e6f', footerLink: 'Careers at IKEA' },
+      { id: 'abt-7g8h9i', footerLink: 'Newsroom' },
+      { id: 'abt-j0k1l2', footerLink: 'Sustainability' },
+      { id: 'abt-3m4n5o', footerLink: 'IKEA Stores' },
+      { id: 'abt-6p7q8r', footerLink: 'IKEA Restaurant' },
+      { id: 'abt-9s0t1u', footerLink: 'IKEA for Business' },
+    ],
+  },
+];
