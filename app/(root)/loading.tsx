fd0f5060b50e4e8221ed8640d13d63675ab69d35
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-07-16 18:21:18 +0530

    ui/loading: add loading ui while pages load

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className='container flex min-h-screen w-full flex-col items-center justify-center bg-white'>
+      <div className='wrapper'>
+        <div className='circle'></div>
+        <div className='shadow'></div>
+      </div>
+    </div>
+  );
+}
