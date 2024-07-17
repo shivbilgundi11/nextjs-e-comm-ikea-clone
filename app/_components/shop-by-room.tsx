9f44c2e6204976fab40a504a6915a32bbd049575
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-07-17 13:03:32 +0530

    ui/navbar: implemented headroom for sticky navmenu

--- app/_components/shop-by-room.tsx
+++ app/_components/shop-by-room.tsx
@@ -4,13 +4,20 @@ import Link from 'next/link';
 import { getImageUrl } from '@/lib/image';
 import { shopByRoomList } from '@/lib/links';
 
+type LinkObject = {
+  id: number;
+  imageURL: string;
+  path: string;
+  roomName: string;
+};
+
 export default function ShopByRoom({ tab }: { tab: number }) {
   return (
     <>
       <div
         className={`${tab === 2 ? 'flex' : 'hidden'} w-max items-center gap-x-2 border-b py-4`}
       >
-        {shopByRoomList.map(({ id, imageURL, path, roomName }) => (
+        {shopByRoomList.map(({ id, imageURL, path, roomName }: LinkObject) => (
           <Link href={path} key={id}>
             <div className='group w-max p-1'>
               <Image
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { MdOutlinePersonOutline } from 'react-icons/md';
@@ -9,63 +11,67 @@ import CreditsAd from '@/app/_components/credits';
 import SearchBox from './search-box';
 import { Button } from './ui/button';
 
+import Headroom from 'react-headroom';
+
 export default function Navbar() {
   return (
     <>
       <header className='flex h-auto w-full flex-col'>
         <CreditsAd />
-        <nav className='container flex min-h-[130px] flex-col items-center justify-evenly border-b md:min-h-[80px] md:flex-row md:justify-between lg:min-h-[90px]'>
-          <div className='flex h-full w-full items-center justify-between'>
-            <div className='flex h-full w-max items-center gap-x-4'>
-              <Link href={'/'}>
-                <Image
-                  src={'/assets/ikea-logo.svg'}
-                  alt='logo'
-                  width={90}
-                  height={36}
-                />
-              </Link>
+        <Headroom>
+          <nav className='container flex min-h-[130px] flex-col items-center justify-evenly border-b bg-white md:min-h-[80px] md:flex-row md:justify-between lg:min-h-[90px]'>
+            <div className='flex h-full w-full items-center justify-between'>
+              <div className='flex h-full w-max items-center gap-x-4'>
+                <Link href={'/'}>
+                  <Image
+                    src={'/assets/ikea-logo.svg'}
+                    alt='logo'
+                    width={90}
+                    height={36}
+                  />
+                </Link>
 
-              <div className='hidden md:block'>
-                <SearchBox />
+                <div className='hidden md:block'>
+                  <SearchBox />
+                </div>
+              </div>
+
+              <div className='flex h-full w-max items-center gap-x-1'>
+                <Link href={'/'}>
+                  <Button
+                    variant={'ghost'}
+                    className='rounded-full p-3 font-normal hover:bg-[#dfdfdf]'
+                  >
+                    <MdOutlinePersonOutline className='mr-2 text-xl' />{' '}
+                    <p className='hidden lg:inline-block'>Hej! Log in</p>
+                  </Button>
+                </Link>
+
+                <Link href={'/favourites'}>
+                  <Button
+                    variant={'ghost'}
+                    className='rounded-full p-3 text-xl font-bold hover:bg-[#dfdfdf]'
+                  >
+                    <PiHeartBold />
+                  </Button>
+                </Link>
+
+                <Link href={'/shoppingcart'}>
+                  <Button
+                    variant={'ghost'}
+                    className='rounded-full p-3 text-xl font-bold hover:bg-[#dfdfdf]'
+                  >
+                    <TbShoppingBag />
+                  </Button>
+                </Link>
               </div>
             </div>
 
-            <div className='flex h-full w-max items-center gap-x-1'>
-              <Link href={'/'}>
-                <Button
-                  variant={'ghost'}
-                  className='rounded-full p-3 font-normal hover:bg-[#dfdfdf]'
-                >
-                  <MdOutlinePersonOutline className='mr-2 text-xl' />{' '}
-                  <p className='hidden lg:inline-block'>Hej! Log in</p>
-                </Button>
-              </Link>
-
-              <Link href={'/favourites'}>
-                <Button
-                  variant={'ghost'}
-                  className='rounded-full p-3 text-xl font-bold hover:bg-[#dfdfdf]'
-                >
-                  <PiHeartBold />
-                </Button>
-              </Link>
-
-              <Link href={'/shoppingcart'}>
-                <Button
-                  variant={'ghost'}
-                  className='rounded-full p-3 text-xl font-bold hover:bg-[#dfdfdf]'
-                >
-                  <TbShoppingBag />
-                </Button>
-              </Link>
+            <div className='block w-full md:hidden'>
+              <SearchBox />
             </div>
-          </div>
-
-          <div className='block w-full md:hidden'>
-            <SearchBox />
-          </div>
-        </nav>
+          </nav>
+        </Headroom>
       </header>
     </>
   );
