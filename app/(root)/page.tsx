9de5af6b5a0d4042d89dc6a21d1e230363560861
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-07-18 18:13:58 +0530

    ui/home: add new at ikea banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '../_components/home-page/hero';
+import NewAtIKEA from '../_components/home-page/new-at-ikea';
 
 export default function Page() {
   return (
     <>
       <main className='h-auto w-full py-8'>
         <Hero />
+        <NewAtIKEA />
       </main>
     </>
   );
---
+++ app/_components/home-page/new-at-ikea.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+
+export default function NewAtIKEA() {
+  return (
+    <>
+      <section className='container flex h-auto w-full flex-col gap-y-3 bg-[#CA5008] p-6 text-white md:p-8 lg:p-12'>
+        <h2 className='text-2xl font-bold'>What&apos;s new at IKEA?</h2>
+        <p className='text-sm'>Breathe new life into your bedroom.</p>
+        <Button
+          variant={'secondary'}
+          className='mt-5 w-max rounded-full text-sm'
+        >
+          Explore More
+        </Button>
+      </section>
+    </>
+  );
+}
